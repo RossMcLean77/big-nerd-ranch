3eed5dd18d8dc9e8edd86a7d8121da1ce0c69b43
Ross McLean <rossmclean.abz@gmail.com>
2017-06-29 17:32:33 +0100

    Chapter 8 Complete

---
+++ CriminalIntent/app/src/main/java/android/bignerdranch/com/criminalintent/Crime.java
@@ -0,0 +1,48 @@
+package android.bignerdranch.com.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Ross on 29/06/2017.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private Boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public Boolean getSolved() {
+        return mSolved;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public void setSolved(Boolean solved) {
+        mSolved = solved;
+    }
+}
